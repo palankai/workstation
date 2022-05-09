a08d82719b77307c483a17172430c0e217a2059c
Csaba Palankai <csaba.palankai@gmail.com>
2022-05-09 19:06:58 +0100

    Fix testing whether the workstation folder exists

--- initial-setup.sh
+++ initial-setup.sh
@@ -61,7 +61,7 @@ export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
 echo "[-] Install cloning workstation setup..."
 mkdir -p ~/opt
 
-if test ! -d "~/opt/workstation" ; then
+if [ ! -d "~/opt/workstation" ] ; then
   echo "[-] Clone workstation setup"
   (cd ~/opt; git clone git@github.com:palankai/workstation.git)
 else
